25898f8069e54889f704300c8ba6eac2d27e63f8
osalvatierra <oscar@owebdesigns.com>
2024-06-25 16:01:47 -0400

    correcting dist location in index for catch-all route, v5

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { Suspense, lazy } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/AuthContext";
