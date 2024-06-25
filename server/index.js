b2950d06f970518016f90831bd166605889acead
osalvatierra <oscar@owebdesigns.com>
2024-06-25 15:48:42 -0400

    correcting dist location in index for catch-all route, v3

--- server/index.js
+++ server/index.js
@@ -450,6 +450,12 @@ app.get("/app/cities/:id", async (req, res) => {
   }
 });
 
+// Log all requests
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 // Serve static files from the React app's build directory
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
