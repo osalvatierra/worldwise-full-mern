090ed8c294422bc5c05d312894d42067f9603526
osalvatierra <oscar@owebdesigns.com>
2024-07-10 13:16:57 -0400

    removing uneccessary files from previous iterations

--- server/routes/quoteRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const QuoteController = require("../controllers/QuoteController");
-const authMiddleware = require("../middleware/authMiddleware");
-
-router.get("/api/quote", authMiddleware, QuoteController.getQuote);
-router.post("/api/quote", authMiddleware, QuoteController.updateQuote);
-
-module.exports = router;
