45310307888369d4791e49cf6d233218e693873c
osalvatierra <oscar@owebdesigns.com>
2025-06-04 12:15:56 -0400

    updating MONGO_URI string to correct DB, and updating mongoos Model Name to User

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose");
 const User = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true },
     quote: { type: String, required: false },
   },
   { collection: "user-data" }
 );
 
-const model = mongoose.model("UserData", User);
+const model = mongoose.model("User", User);
 
 module.exports = model;
